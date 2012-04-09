f9b11a18ebe02d13864c0915fb863c39a8188ceb
Radko Sabol <radko.sabol@gmail.com>
2012-04-09 13:11:35 +0200

    Drobne upravy v triede ObjectDetection - jednoduche okomentovanie zdrojoveho kodu - vymazanie nepotrebnych casti zdrojoveho kodu

--- object_detection.cpp
+++ object_detection.cpp
@@ -2,36 +2,21 @@
 * DEV
 */
 
-// Vlastny hlavickovy subor
 #include "object_detection.hpp"
 
 /**
-* Circle
-*/
-/*
-void ObjectDetection::cvOpen(const CvArr *src, CvArr *dst, IplConvKernel *element)
-{
-	cvErode (src, dst, element, 1);
-	cvDilate(src, dst, element, 1);
-}
-*/
-
-/*
-void ObjectDetection::cvClose(const CvArr *src, CvArr *dst, IplConvKernel *element)
-{
-	cvDilate(src, dst, element, 1);
-	cvErode (src, dst, element, 1);
-}
-*/
-
-void ObjectDetection::circleDetectObjects(cv::Mat& img) // vela falosnych detekcii
+ * Detekcia kruhov v obraze
+ *
+ * @param cv::Mat &img - obraz, v ktorom su detegovane kruhy
+ */
+void ObjectDetection::circleDetectObjects(cv::Mat &img) // TODO: vyladit nastavenie farieb lebo je vela falosnych detekcii
 {
   cv::Mat gray, gray2, imageHSV;
   //cvtColor(img, gray, CV_BGR2GRAY);
   cv::cvtColor(img, imageHSV, CV_BGR2HSV);
-  cv::inRange(imageHSV, cv::Scalar(0, 50, 170, 0), cv::Scalar(10, 180, 256, 0), gray);
-  cv::inRange(imageHSV, cv::Scalar(170, 50, 170, 0), cv::Scalar(256, 180, 256, 0), gray2);
-  cv::bitwise_or(gray, gray2, gray);
+  cv::inRange(imageHSV, cv::Scalar(0, 50, 170, 0), cv::Scalar(10, 180, 256, 0), gray); // prahovanie jednym rozsahom farieb
+  cv::inRange(imageHSV, cv::Scalar(170, 50, 170, 0), cv::Scalar(256, 180, 256, 0), gray2); // prahovanie druhym rozsahom farieb
+  cv::bitwise_or(gray, gray2, gray); // spojenie prahovanych obrazov
   // smooth it, otherwise a lot of false circles may be detected
   cv::GaussianBlur(gray, gray, cv::Size(9, 9), 1, 1);
   cv::Canny(gray, gray, 1, 3);
@@ -62,106 +47,24 @@ void ObjectDetection::circleDetectObjects(cv::Mat& img) // vela falosnych detekc
   //cv::imshow( "circles", gray );
 }
 
-/*
-void ObjectDetection::circleDetectAndDrawObjectsC(const IplImage* imageMain) // nefunguje podla mojich predstav
-{
-  CvSize imageSize = cvGetSize(imageMain);
-  int imageDepth = imageMain->depth,
-      imageChannels = imageMain->nChannels;
 
-  IplImage* imageCircle = cvCreateImage(imageSize, imageDepth, imageChannels); // klon vstupneho obrazka
-  cvCopy(imageMain, imageCircle, NULL); // skopirovanie obrazka do novej premennej
-
-  IplImage* imageHSV = cvCreateImage(imageSize, imageDepth, imageChannels); // farbeny obrazok v HSV
-  //IplImage* imageTresholded1 = cvCreateImage(imageSize, imageDepth, 1); // ciernobiely obrazok
-  //IplImage* imageTresholded2 = cvCreateImage(imageSize, imageDepth, 1); // ciernobiely obrazok
-
-  /** Vytvorenie masky *-/
-  CvMat *mask = cvCreateMat(imageSize.height, imageSize.width, CV_8UC1);
-  CvMat *imageTresholded1 = cvCreateMat(imageSize.height, imageSize.width, CV_8UC1);
-  CvMat *imageTresholded2 = cvCreateMat(imageSize.height, imageSize.width, CV_8UC1);
-
-  /** Konverzia obrazu do HSV*-/
-  cvCvtColor(imageCircle, imageHSV, CV_BGR2HSV);
-
-  /** Prahovanie farieb na Aibo-vu lopticku *-/
-  CvScalar hsv_min = cvScalar(0, 50, 170, 0);
-  CvScalar hsv_max = cvScalar(10, 180, 256, 0);
-  CvScalar hsv_min2 = cvScalar(170, 50, 170, 0);
-  CvScalar hsv_max2 = cvScalar(256, 180, 256, 0);
-
-  cvInRangeS(imageHSV, hsv_min, hsv_max, imageTresholded1);
-  cvInRangeS(imageHSV, hsv_min2, hsv_max2, imageTresholded2);
-  cvOr(imageTresholded1, imageTresholded2, imageTresholded1);
-
-  /** Prahovanie farby tenisovej lopticky 
-  cvInRangeS(imageHSV, cvScalar(0.11*256, 0.60*256, 0.20*256, 0),
-	                     cvScalar(0.14*256, 1.00*256, 1.00*256, 0), mask);
-  *-/
-  cvReleaseImage(&imageHSV);
-
-  /* Perform morphological ops *-/
-	IplConvKernel *se21 = cvCreateStructuringElementEx(21, 21, 10, 10, CV_SHAPE_RECT, NULL);
-	IplConvKernel *se11 = cvCreateStructuringElementEx(11, 11, 5,  5,  CV_SHAPE_RECT, NULL);
-	cvClose(imageTresholded1, imageTresholded1, se21); // See completed example for cvClose definition
-	cvOpen(imageTresholded1, imageTresholded1, se11);  // See completed example for cvOpen  definition
-	cvReleaseStructuringElement(&se21);
-	cvReleaseStructuringElement(&se11);
-
-  /* Copy mask into a grayscale image *-/
-  /*
-	IplImage *hough_in = cvCreateImage(size, 8, 1);
-	cvCopy(mask, hough_in, NULL);
-  cvSmooth(hough_in, hough_in, CV_GAUSSIAN, 15, 15, 0, 0);
-  *-/
-  //cvSmooth(imageTresholded1, imageTresholded1, CV_GAUSSIAN, 9, 9, 0, 0);
-  cvCanny(imageTresholded1, imageTresholded1, (float)1, (float)1*3, 3);
-  
-
-  //cvSmooth(imageTresholded1, imageTresholded1, CV_GAUSSIAN, 15, 15, 0, 0);
-
-    /* Run the Hough function *-/
-	CvMemStorage *storage = cvCreateMemStorage(0);
-  CvSeq *circles = cvHoughCircles(imageTresholded1, storage, CV_HOUGH_GRADIENT, 1, imageTresholded1->height/10, 100, 40, 0, 0);
-	cvReleaseMemStorage(&storage);
-
-  cvShowImage("Video ball tresholded #n", imageTresholded1);
-  //std::cout << circles->total << std::endl;
-
-	for (int i = 0; i < circles->total; i++) 
-  {
-    std::cout << "Circle detected" << std::endl;
-    float *p = (float*)cvGetSeqElem(circles, i);
-    CvPoint center = cvPoint(cvRound(p[0]),cvRound(p[1]));
-    CvScalar val = cvGet2D(mask, center.y, center.x);
-    if (val.val[0] < 1) continue;
-    //cvCircle(imageCircle,      center, 3,             CV_RGB(0,255,0), -1, CV_AA, 0);
-    //cvCircle(imageCircle,      center, cvRound(p[2]), CV_RGB(255,0,0),  3, CV_AA, 0);
-    cvCircle(imageTresholded1, center, 3,             CV_RGB(0,255,0), -1, CV_AA, 0);
-    cvCircle(imageTresholded1, center, cvRound(p[2]), CV_RGB(255,0,0),  3, CV_AA, 0);
-	}
-
-    
-
-  //cvSetMouseCallback("Video ball tresholded #1", onMouseClick, imageMain);
-  //cvShowImage("Video circle test", imageCircle);
-  //cvShowImage("Video circle tresholded #1", imageTresholded1);
-  //cvShowImage("Video ball tresholded #2", imageBallTresholded2);
-  
-}
-*/
 
 /**
-* The "Square Detector" program.
-* It loads several images sequentially and tries to find squares in each image
-* Zdroj: http://stackoverflow.com/questions/7731742/square-detection-doesnt-find-squares
-*/
+ * Detektor obdlznikovych objektov v obraze
+ *
+ * Zdroj: http://stackoverflow.com/questions/7731742/square-detection-doesnt-find-squares
+ */
 int thresh = 50, N = 2; // karlphillip: decreased N to 2, was 11.
-const char* wndname = "Square Detection Demo";
 
-// helper function:
-// finds a cosine of angle between vectors
-// from pt0->pt1 and from pt0->pt2
+/**
+ * Detektor obdlznikovych objektov v obraze - pomocna funkcia
+ *   - hlada kosinus medzi vektormi pt0->pt1 a pt0->pt2
+ *
+ * @param cv::Point pt1
+ * @param cv::Point pt2
+ * @param cv::Point pt0
+ * Zdroj: http://stackoverflow.com/questions/7731742/square-detection-doesnt-find-squares
+ */
 double ObjectDetection::angle(cv::Point pt1, cv::Point pt2, cv::Point pt0 )
 {
   double dx1 = pt1.x - pt0.x;
@@ -171,14 +74,18 @@ double ObjectDetection::angle(cv::Point pt1, cv::Point pt2, cv::Point pt0 )
   return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
 }
 
-// returns sequence of squares detected on the image.
-// the sequence is stored in the specified memory storage
-void ObjectDetection::squareDetectObjects(const cv::Mat& image)
+/**
+ * Detektor obdlznikovych objektov v obraze
+ *
+ * @param const cv::Mat &image - obraz, v ktorom su detegovane obdlznikove predmety
+ * Zdroj: http://stackoverflow.com/questions/7731742/square-detection-doesnt-find-squares
+ */
+void ObjectDetection::squareDetectObjects(const cv::Mat &image)
 {
   /** Deklaracia premennych */
   cv::Mat pyr,
-  gray0(image.size(), CV_8U),
-  gray;
+          gray0(image.size(), CV_8U),
+          gray;
   //cv::Mat image2 = image.clone(); // naklonovanie vstupneho obrazu
   //CvMat imageCvMat = image2; // konverzia z cv::Mat do CvMat
     
@@ -258,7 +165,7 @@ void ObjectDetection::squareDetectObjects(const cv::Mat& image)
             maxCosine = MAX(maxCosine, cosine);
           }
 
-          for (int k = 0; k < approx.size(); k++)
+          for (unsigned int k = 0; k < approx.size(); k++)
           {
             approxS[k] = cv::Scalar(approx[k].x, approx[k].y, 0, 0);
           }
@@ -282,96 +189,24 @@ void ObjectDetection::squareDetectObjects(const cv::Mat& image)
   //std::cout << " ... " << objects.size() << std::endl;
 }
 
-// the function draws all the squares in the image
-/*
-void ObjectDetection::squareDrawObjects(cv::Mat& image, const std::vector<std::vector<cv::Point>>& squares)
-{
-    for(size_t i = 0; i < squares.size(); i++)
-    {
-        const cv::Point* p = &squares[i][0];
-        int n = (int)squares[i].size();
-        cv::polylines(image, &p, &n, 1, true, cv::Scalar(0, 255, 0), 3, CV_AA);
-
-        // Vypocet stredoveho bodu
-        int x = 0,
-            y = 0;
-
-        x = ((squares[i][0].x + squares[i][2].x)/2 + (squares[i][1].x + squares[i][3].x)/2) / 2;
-        y = ((squares[i][0].y + squares[i][2].y)/2 + (squares[i][1].y + squares[i][3].y)/2) / 2;
-
-        cv::circle(image, cv::Point(squares[i][0].x, squares[i][0].y), 2, cv::Scalar(0, 0, 255), 1);
-        //cv::circle(image, cv::Point(squares[i][1].x, squares[i][1].y), 2, cv::Scalar(255, 0, 0), 1);
-        cv::circle(image, cv::Point(squares[i][2].x, squares[i][2].y), 2, cv::Scalar(255, 0, 255), 1);
-        cv::circle(image, cv::Point(x, y), 2, cv::Scalar(255, 0, 0), 1);
-    }
-
-    cv::imshow(wndname, image);
-}
-*/
-
 /**
-* Haar
-*/
-void ObjectDetection::loadHaarObjectDetector(std::string cascadePath)
+ * Nacitanie haar kaskad
+ *
+ * @param std::string cascadePath - cesta ku suboru s haar kaskadami
+ * @return boolean - indikator, ci boli kaskady uspesne nacitane
+ */
+boolean ObjectDetection::loadHaarObjectDetector(std::string cascadePath)
 {
     qDebug() << QString(cascadePath.c_str());
-    if (! m_haarCascade.load(cascadePath)) { qDebug() << "ERROR"; } // C++
-  // m_cascade = (CvHaarClassifierCascade*)cvLoad(cascadePath); // C
+    if (! m_haarCascade.load(cascadePath)) { qDebug() << "Pri nacitani haar kaskad sa vyskytla chyba"; return false; } else { qDebug() << "Haar kaskady uspesne nacitane"; return true; } // C++
 }
 
-/*
-void ObjectDetection::haarDetectAndDrawObjectsC(IplImage* image, CvHaarClassifierCascade* cascade, int do_pyramids, std::vector<std::vector<std::vector<cv::Scalar>>>& objects)
-{
-  CvSize size = cvGetSize(image);
-  IplImage* imageCircle = cvCreateImage(size, image->depth, image->nChannels);
-  cvCopy(image, imageCircle, NULL);
-  IplImage* smallImage = imageCircle;
-  CvMemStorage* storage = cvCreateMemStorage(0);
-  CvSeq* foundObjects;
-  int i,
-      scale = 1;
-
-  /* if the flag is specified, down-scale the input image to get a
-      performance boost w/o loosing quality (perhaps) *-/
-  if(do_pyramids)
-  {
-    smallImage = cvCreateImage(cvSize(imageCircle->width/2, imageCircle->height/2), IPL_DEPTH_8U, 3);
-    cvPyrDown(image, smallImage, CV_GAUSSIAN_5x5);
-    scale = 2;
-  }
-  //cvCvtColor(smallImage, smallImage, CV_BGR2GRAY);
-  /* use the fastest variant *-/
-  foundObjects = cvHaarDetectObjects(smallImage, cascade, storage, 1.2, 2, CV_HAAR_DO_CANNY_PRUNING, cvSize(0, 0));
-  //foundObjects = cvHaarDetectObjects(smallImage, cascade, storage, 1.2, 2, CV_HAAR_DO_ROUGH_SEARCH, cvSize(0, 0));
-  
-  /* draw all the rectangles *-/
-  for(i = 0; i < foundObjects->total; i++)
-  {
-    /* extract the rectanlges only *-/
-    CvRect objectRect = *(CvRect*)cvGetSeqElem(foundObjects, i);
-    //if ((objectRect.width > 20) && (objectRect.width < 40))
-    //{
-      std::cout << "Haar object detected" << std::endl;
-      std::cout << "Objekt cislo: " << (i + 1) << " - X: " << objectRect.x << "; Y: " << objectRect.y << "; height: " << objectRect.height << "; width: " << objectRect.width << std::endl << std::endl;;
-      //cvRectangle(image, cvPoint(face_rect.x*scale, face_rect.y*scale), cvPoint((face_rect.x+face_rect.width)*scale, (face_rect.y+face_rect.height)*scale), CV_RGB(255,0,0), 3);
-      /*
-      cv::Point center(objectRect.x + objectRect.width*0.5, objectRect.y + objectRect.height*0.5);
-      cvEllipse(imageCircle, center, cvSize(objectRect.width*0.5, objectRect.height*0.5), 0, 0, 360, cvScalar(255, 0, 0), 2, 8, 0 );
-      *-/
-
-      objects[0].resize(objects[0].size()+1);
-      objects[0][objects[0].size()-1].resize(1);
-      objects[0][objects[0].size()-1][0] = cv::Scalar(objectRect.x, objectRect.y, objectRect.width, objectRect.height);
-    //}
-  }
-
-  //cvShowImage("Video haar", imageCircle);
-  if (smallImage != imageCircle) { cvReleaseImage(&smallImage); }
-  cvReleaseImage(&imageCircle);
-  cvReleaseMemStorage(&storage);
-}
-*/
-
+/**
+ * Haar detektor objektov
+ *
+ * @param cv::Mat image - obraz, v ktorom su hladane objekty z haar kaskady
+ * Zdroj: http://opencv.itseez.com/doc/tutorials/objdetect/cascade_classifier/cascade_classifier.html
+ */
 void ObjectDetection::haarDetectObjects(cv::Mat image)
 {
   std::vector<cv::Rect> foundObjects;
@@ -384,14 +219,17 @@ void ObjectDetection::haarDetectObjects(cv::Mat image)
 
   for (size_t i = 0; i < foundObjects.size(); i++)
   {
-    qDebug() << "detected " << i;
     m_objects[0].resize(m_objects[0].size()+1); // alokacia miesta pre najdeny objekt
     m_objects[0][m_objects[0].size()-1].resize(1); // alokacia miesta pre suradnice objektu (zapisuje sa len do prveho prvku, ale potrebujem mat taketo pole aby som vedel pri hladani obdlznikov zapisovat suradnice vsetkcyh rohov (nie je tam 90°, tak musim kazdy bod osobitne))
     m_objects[0][m_objects[0].size()-1][0] = cv::Scalar(foundObjects[i].x, foundObjects[i].y, foundObjects[i].width, foundObjects[i].height);
   }
 }
 
-
+/**
+ * Zakreslenie najdenych objektov (premenna m_objects) do obrazu imageMat
+ *
+ * @param cv::Mat &imageMat - obraz, do ktoreho sa zakreslia najdene objekty
+ */
 void ObjectDetection::drawDetectedObjects(cv::Mat &imageMat)
 {
   for (size_t i = 0; i < m_objects.size(); i++) // ID identifikatora
@@ -403,7 +241,7 @@ void ObjectDetection::drawDetectedObjects(cv::Mat &imageMat)
         cv::rectangle(imageMat, cv::Point(m_objects[i][j][0][0], m_objects[i][j][0][1]), cv::Point(m_objects[i][j][0][0] + m_objects[i][j][0][2], m_objects[i][j][0][1] + m_objects[i][j][0][3]), cv::Scalar(0, 255, 0), 3, 8, 0);
         cv::Point center(m_objects[i][j][0][0] + m_objects[i][j][0][2]*0.5, m_objects[i][j][0][1] + m_objects[i][j][0][3]*0.5);
         cv::circle(imageMat, center, 1, cv::Scalar(255, 0, 0), 3, 8, 0);
-        std::cout << "X: " << center.x << " ; Y: " << center.y << std::endl;
+        //std::cout << "X: " << center.x << " ; Y: " << center.y << std::endl;
           
         /*
 	      if ((center.x > 0) && (center.y > 0))
@@ -462,11 +300,17 @@ void ObjectDetection::drawDetectedObjects(cv::Mat &imageMat)
   //cv::imshow("Video detected", imageMat);
 }
 
+/**
+ *
+ */
 std::vector<std::vector<cv::Scalar>> ObjectDetection::getObjects(int index)
 {
   return m_objects[index];
 }
 
+/**
+ * Vyprazdnenie premennej obsahujucej informacie o najdenych objektoch
+ */
 void ObjectDetection::clearObjects()
 {
     m_objects.clear();
--- object_detection.hpp
+++ object_detection.hpp
@@ -2,12 +2,13 @@
 * DEV
 */
 
-// Standardne hlavickove subory
+
 #include <iostream>
 #include <math.h>
 #include <string.h>
+
 #include <QDebug>
-// OpenCV hlavickove subory
+
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
@@ -18,31 +19,23 @@ class ObjectDetection
     std::vector<std::vector<std::vector<cv::Scalar>>> m_objects;
     cv::CascadeClassifier m_haarCascade;
 
-    // circle
-    //void cvOpen(const CvArr *src, CvArr *dst, IplConvKernel *element);
-    //void cvClose(const CvArr *src, CvArr *dst, IplConvKernel *element);
-
     // square
     double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0 );
 
   public:
     // circle
-    //void circleDetectAndDrawObjectsC(const IplImage* imageMain);
     void circleDetectObjects(cv::Mat& img);
 
     // square
     void squareDetectObjects(const cv::Mat& image);
-    //void squareDrawObjects(cv::Mat& image, const std::vector<std::vector<cv::Point>>& squares);
 
     // haar
-    void loadHaarObjectDetector(std::string cascadePath);
-    //void haarDetectAndDrawObjectsC(IplImage* image, CvHaarClassifierCascade* cascade, int do_pyramids, std::vector<std::vector<std::vector<cv::Scalar>>>& objects);
+    boolean loadHaarObjectDetector(std::string cascadePath);
     void haarDetectObjects(cv::Mat image);
 
     // vykreslenie najdenych objektov
     void ObjectDetection::drawDetectedObjects(cv::Mat &image);
 
     std::vector<std::vector<cv::Scalar>> ObjectDetection::getObjects(int index);
-
     void clearObjects();
 };
